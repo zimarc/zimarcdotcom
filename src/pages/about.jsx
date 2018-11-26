f99ebcda7784ceb6835a983cae4d4bb7d7a7d918
maysssam@gmail.com <maysssam@gmail.com>
2018-11-26 13:38:16 -0800

    about me

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -12,16 +12,12 @@ const About = () => (
       </>
     </Header>
     <Container type="text">
-      <h2>Hi!</h2>
-      <p>
-        Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind
-        texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean. A
-        small river named Duden flows by their place and supplies it with the necessary regelialia. It is a
-        paradisematic country, in which roasted parts of sentences fly into your mouth. Even the all-powerful Pointing
-        has no control about the blind texts it is an almost unorthographic life One day however a small line of blind
-        text by the name of Lorem Ipsum decided to leave for the far World of Grammar. The Big Oxmox advised her not to
-        do so, because there were thousands.
-      </p>
+    <h1 >Hi! We Are Zimarc from Los Angeles</h1>
+					<p> 
+						Zimarc is an experimental architecture design studio focus on innovative built 
+						environment and exploring design efficiency through 
+						digital fabrication and advanced manufacturing.
+					</p>
     </Container>
   </Layout>
 );
