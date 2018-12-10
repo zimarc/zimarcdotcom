367258eee49a83342eb731b57f6c8c4ffaaa0f85
maysssam@gmail.com <maysssam@gmail.com>
2018-12-10 14:14:35 -0800

    svg

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,6 +36,12 @@ module.exports = {
             },
           },
           {
+            resolve: "gatsby-remark-copy-linked-files",
+            options: {
+              ignoreFileExtensions: [`png`, `jpg`, `jpeg`, `bmp`, `tiff`],
+            },
+          },
+          {
             resolve: 'gatsby-remark-external-links',
             options: {
               target: '_blank',
